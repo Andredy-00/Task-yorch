f653e04e8f0985a2259b00128a29a175693490f7
andredy-00 <andredyguerra00@gmail.com>
2026-02-08 02:17:02 -0500

    Feat: AuthContext

--- actions/auth/get-user.ts
+++ actions/auth/get-user.ts
@@ -1,28 +1,29 @@
+import { User } from "@/interfaces/user";
 import { createClient } from "@/lib/supabase/server";
 
-export const getUser = async () => {
+export const getUser = async (): Promise<User | null> => {
+  try {
+    const supabase = await createClient();
+    const {
+      data: { user: session },
+    } = await supabase.auth.getUser();
 
-    try {
-        const supabase = await createClient();
-   const { data: { user: session } } = await supabase.auth.getUser();
-   
-   if (!session) return null;
+    if (!session) return null;
 
-   const { data: userData, error: userError } = await supabase
-    .from('profiles')
-    .select('*')
-    .eq('id', session.id)
-    .single();
+    const { data: userData, error: userError } = await supabase
+      .from("profiles")
+      .select("*")
+      .eq("id", session.id)
+      .single();
 
-    if(userError){
-        console.log("Error dentro featching user: ", userError);
-        return null
+    if (userError) {
+      console.log("Error dentro featching user: ", userError);
+      return null;
     }
 
     return userData;
-    } catch (error) {
-        console.log("Error fuera featching user: ", error);
-        return null;
-    }
-
-}
+  } catch (error) {
+    console.log("Error fuera featching user: ", error);
+    return null;
+  }
+};
